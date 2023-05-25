b5c4b0f591a2c5f2b2a40cf338f19cce8a065e58
jmaxwell <jmaxwell@jlab.org>
2023-05-25 10:33:42 -0400

    Sig plots working

--- app/gui_history.py
+++ app/gui_history.py
@@ -5,8 +5,11 @@ import glob
 import json
 import numpy as np
 from PyQt5.QtWidgets import QWidget, QLabel, QGroupBox, QHBoxLayout, QVBoxLayout, QGridLayout, QLineEdit, QSpacerItem, \
-    QSizePolicy, QComboBox, QPushButton, QProgressBar, QStackedWidget, QDoubleSpinBox, QDateTimeEdit, QListWidget
+    QTableView, QAbstractItemView, QAbstractScrollArea, QStackedWidget, QDoubleSpinBox, QDateTimeEdit, QPushButton
+from PyQt5.QtGui import QIntValidator, QDoubleValidator, QValidator, QStandardItemModel, QStandardItem
+from PyQt5.QtCore import Qt
 import pyqtgraph as pg
+from dateutil.parser import parse
 
 
 class HistTab(QWidget):
@@ -22,12 +25,12 @@ class HistTab(QWidget):
         pg.setConfigOption('foreground', 'k')
 
 
-        self.base_pen = pg.mkPen(color=(180, 0, 0), width=1.5)
-        self.base2_pen = pg.mkPen(color=(0, 0, 150), width=1.5)
-        self.base3_pen = pg.mkPen(color=(0, 180, 0), width=1.5)
-        self.sub_pen = pg.mkPen(color=(180, 0, 0), width=1.5)
-        self.sub2_pen = pg.mkPen(color=(0, 0, 150), width=1.5)
-        self.sub3_pen = pg.mkPen(color=(0, 180, 0), width=1.5)
+        self.base_pen = pg.mkPen(color=(180, 0, 0), width=2)
+        self.base2_pen = pg.mkPen(color=(0, 0, 180), width=2)
+        self.base3_pen = pg.mkPen(color=(0, 180, 0), width=2)
+        self.sub_pen = pg.mkPen(color=(220, 0, 0), width=2)
+        self.sub2_pen = pg.mkPen(color=(0, 0, 150), width=2)
+        self.sub3_pen = pg.mkPen(color=(0, 180, 0), width=2)
 
         self.main = QHBoxLayout()  # main layout
         self.setLayout(self.main)
@@ -39,42 +42,55 @@ class HistTab(QWidget):
         # Datetime box box
         self.date_box = QGroupBox('Datetime Range Selection')
         self.date_box.setLayout(QHBoxLayout())
-        self.start_label = QLabel("Start")
+        self.start_label = QLabel("Start:")
+        self.start_label.setAlignment(Qt.AlignCenter)
         self.date_box.layout().addWidget(self.start_label)
         self.left.addWidget(self.date_box)
         self.start_dedit = QDateTimeEdit(calendarPopup=True)
         self.end_dedit = QDateTimeEdit(calendarPopup=True)
-        self.start_dedit.setDateTime(datetime.datetime.strptime('Jun 1 2022  12:00AM', '%b %d %Y %I:%M%p'))
+        self.start_dedit.setDateTime(datetime.datetime.strptime('Mar 31 2023  12:00AM', '%b %d %Y %I:%M%p'))
         self.end_dedit.setDateTime(datetime.datetime.strptime('Apr 1 2023  12:00AM', '%b %d %Y %I:%M%p'))
-        self.start_dedit.dateTimeChanged.connect(self.range_changed)
+        #self.start_dedit.dateTimeChanged.connect(self.range_changed)
         self.date_box.layout().addWidget(self.start_dedit)
-        self.end_label = QLabel("End")
+        self.end_label = QLabel("End:")
+        self.end_label.setAlignment(Qt.AlignCenter)
         self.date_box.layout().addWidget(self.end_label)
-        self.end_dedit.dateTimeChanged.connect(self.range_changed)
+        #self.end_dedit.dateTimeChanged.connect(self.range_changed)
         self.date_box.layout().addWidget(self.end_dedit)
+        self.range_but = QPushButton('Open Range')
+        self.range_but.clicked.connect(self.range_changed)
+        self.date_box.layout().addWidget(self.range_but)
 
-        #  Event box
-        self.var_box = QGroupBox('Event Selection')
-        self.var_box.setLayout(QVBoxLayout())
-        self.left.addWidget(self.var_box)
-        self.var_list = QListWidget()
-        self.var_list.setSelectionMode(QListWidget.MultiSelection)
-        self.var_list.itemSelectionChanged.connect(self.update_plot)
-        self.var_box.layout().addWidget(self.var_list)
+        # Selection list
+        self.event_model = QStandardItemModel()
+        self.event_model.setHorizontalHeaderLabels(
+            ['Timestamp', 'Date', 'Time', 'Polarization', 'Sweeps', 'Channel', 'Label'])
+
+        self.event_table = QTableView()
+        self.event_table.setSelectionBehavior(QAbstractItemView.SelectRows)
+        self.event_table.setSizeAdjustPolicy(QAbstractScrollArea.AdjustToContents)
+        self.event_table.resizeColumnsToContents()
+        self.event_table.setModel(self.event_model)
+        self.event_table.clicked.connect(self.select_event)
+        self.left.addWidget(self.event_table)
 
         # Right Side
         self.right = QVBoxLayout()
         self.main.addLayout(self.right)
 
         self.time_axis = pg.DateAxisItem(orientation='bottom')
-        self.strip_wid = pg.PlotWidget(title='Selected Polarization History', axisItems={'bottom': self.time_axis})
+        self.strip_wid = pg.PlotWidget(title='Range Polarization History', axisItems={'bottom': self.time_axis})
         self.strip_wid.showGrid(True, True)
         self.strip_wid.addLegend(offset=(0.5, 0))
+        self.strip_plot = self.strip_wid.plot([], [], pen=self.base2_pen, name='Polarization')
         self.right.addWidget(self.strip_wid)
 
         self.sig_wid = pg.PlotWidget(title='Selected Signal')
         self.sig_wid.showGrid(True, True)
-        self.sig_plot = self.sig_wid.plot([], [], pen=self.sub_pen)
+        self.raw_plot = self.sig_wid.plot([], [], pen=self.sub_pen, name='Raw')
+        self.sub_plot = self.sig_wid.plot([], [], pen=self.sub2_pen, name='Base Subtracted')
+        self.fin_plot = self.sig_wid.plot([], [], pen=self.sub3_pen, name='Fit Subtracted')
+        self.sig_wid.addLegend(offset=(0.5, 0))
         self.right.addWidget(self.sig_wid)
 
 
@@ -84,7 +100,6 @@ class HistTab(QWidget):
         self.start = self.start_dedit.dateTime().toPyDateTime()
         self.end = self.end_dedit.dateTime().toPyDateTime()
         self.all_files = glob.glob(f"{self.parent.settings['proton_data_dir']}/*.txt")
-        self.current_name = ''
         self.current_time = datetime.datetime.strptime('Jan 1 2000  12:00AM', '%b %d %Y %I:%M%p')
         self.included = []
         for file in self.all_files:
@@ -92,51 +107,58 @@ class HistTab(QWidget):
             if 'current' in name or 'baseline' in name:
                 continue
             else:
-                print(name)
                 start, stop = name.split('__')
                 stop = stop.replace('.txt', '')
                 start_dt = datetime.datetime.strptime(start, "%Y-%m-%d_%H-%M-%S")
                 stop_dt = datetime.datetime.strptime(stop, "%Y-%m-%d_%H-%M-%S")
                 if self.start < start_dt < self.end or self.start < stop_dt < self.end:
                     self.included.append(file)
-        self.included.append(self.current_name)
-        # [print(l) for l in self.included]
-
-        load = {}
+        self.all = {}
         for eventfile in self.included:
+            print('Parsing file:', eventfile)
             with open(eventfile, 'r') as f:
                 for line in f:
-                    temp = json.loads(line)
-                    s = temp['stop_time']
+                    event = json.loads(line)
+                    s = event['stop_time']
                     line_stoptime = datetime.datetime.strptime(s[:26], '%Y-%m-%d %H:%M:%S.%f')
-                    load[line_stoptime] = {}
-                    if self.start < line_stoptime < self.end:
-                        for k in temp.keys():
-                            if k in self.event_vars_included:
-                                load[line_stoptime][k] = temp[k]
-                            if 'epics_reads' in k:
-                                for key, val in temp[k].items():
-                                    load[line_stoptime][key] = val
-        self.data = {}
-        for time in load.keys():  # take loaded data in datetime:var:value and make var:numpy2d dict
-            for var in load[time].keys():
-                try:
-                    self.data[var] = np.concatenate((self.data[var], np.array([[time.timestamp(), load[time][var]]])))
-                except KeyError:
-                    self.data[var] = np.array([[time.timestamp(), load[time][var]]])
+                    utcstamp = str(event['stop_stamp'])
+                    if self.start < line_stoptime < self.end and 'pol' in event:
+                        self.all[utcstamp] = event    # full dictionary from datafile
 
-        self.var_list.clear()
-        self.var_list.addItems(self.data.keys())
+        self.event_model.removeRows(0, self.event_model.rowCount())
+        for i, stamp in enumerate(self.all.keys()):
+            try:
+                dt = parse(self.all[stamp]['stop_time'])
+                time = dt.strftime("%H:%M:%S")
+                date = dt.strftime("%m/%d/%y")
+                self.event_model.setItem(i,0,QStandardItem(str(self.all[stamp]['stop_stamp'])))
+                self.event_model.setItem(i,1,QStandardItem(date))
+                self.event_model.setItem(i,2,QStandardItem(time))
+                self.event_model.setItem(i,3,QStandardItem(f"{self.all[stamp]['pol']:.4f}"))
+                self.event_model.setItem(i,4,QStandardItem(str(self.all[stamp]['sweeps'])))
+                self.event_model.setItem(i,5,QStandardItem(str(self.all[stamp]['channel']['name'])))
+                self.event_model.setItem(i,6,QStandardItem(str(self.all[stamp]['label'])))
+            except KeyError:
+                pass
+        try:
+            graph_data = np.column_stack((list([float(k) for k in sorted(self.all.keys())]),[float(self.all[k]['pol']) for k in sorted(self.all.keys())]))
+            self.strip_plot.setData(graph_data)
+        except KeyError:
+            pass
 
-    def update_plot(self):
-        '''Update strip plots.
+
+    def select_event(self, item):
+        self.update_event_plot(item)
+
+    def update_event_plot(self, item):
+        '''Update event plot.
         '''
-        self.strip_wid.clear()
-        self.strip_plot = {}
-        i = 0
-        for var in self.var_list.selectedItems():
-            self.strip_plot[var.text()] = self.strip_wid.plot([], [], name=var.text(), pen=pg.mkPen(i))
-            i += 1
-            # print(self.data[var.text()])
-            self.strip_plot[var.text()].setData(self.data[var.text()])
+        stamp = self.event_model.data(self.event_model.index(item.row(), 0))
+        freq_list = np.array(self.all[stamp]['freq_list'])
+        phase = np.array(self.all[stamp]['phase'])
+        sub = np.array(self.all[stamp]['basesub'])
+        fin = np.array(self.all[stamp]['fitsub'])
+        self.raw_plot.setData(freq_list,phase-phase.max())
+        self.sub_plot.setData(freq_list,sub-sub.max())
+        self.fin_plot.setData(freq_list,fin)
 
