a4881a13ee3dc7f21b9c3bc6bf152fd645a2e87a
jmaxwell <jmaxwell@jlab.org>
2023-07-06 15:48:37 -0400

    Scraper fixes

--- app/scraper.py
+++ app/scraper.py
@@ -14,42 +14,49 @@ def main():
     utc = tz.gettz('UTC')
 
     settings = load_settings()
+    print("Loading BCMs")
     bcms = read_bcms()   # keyed on datetime
     runs = read_runs()             # keyed on run  number, value is dict with 'start_time', 'stop_time', 'species'
+    print("Loading events")
+    events = get_events(settings)
     moller_runs = read_mollers()   # keyed on dt, value is pol
     scatter_runs = read_scattering() # keyed on run number, value is pol
-
+    print("Finished loads")
 
     for run in sorted(runs.keys()):   # get dose for this event
-        events = select_events(settings, runs[run]['start_time'], runs[run]['stop_time'])
+        print("Run", run, runs[run]['start_time'], runs[run]['stop_time'])
+        selected = select_events(events, runs[run]['start_time'], runs[run]['stop_time'])
         if '20' in runs[run]['cell']:
-            raster_area = np.pi*9*9  # assuming 18mm raster
+            raster_area = np.pi*0.9*0.9  # assuming 18mm raster
         elif '15' in runs[run]['cell']:
-            raster_area = np.pi*6.5*6.5  # assuming 13mm raster
+            raster_area = np.pi*0.65*0.65  # assuming 13mm raster
         else:
             print('No cell match')
             raster_area = 1
         weighted_pol = 0
         weight = 0
         # Charge average pol per run
-        for event in sorted(events):
+        for event in sorted(selected.keys()):
             sum_charge = 0
-            event_start = datetime.fromtimestamp(float(event['start_stamp']),utc)
-            event_stop = datetime.fromtimestamp(float(event['stop_stamp']),utc)
+            event_start = datetime.fromtimestamp(float(selected[event]['start_stamp']),utc)
+            event_stop = datetime.fromtimestamp(float(selected[event]['stop_stamp']),utc)
             include_bcms = []
             for dt in bcms.keys():  # find bcms to include
                 if event_start < dt < event_stop:
                     include_bcms.append(dt)   #
             previous = 0
             for dt in sorted(include_bcms):  # do time weighted sum
-                if previous = 0: previous = dt
-                if dt-previous > 60: # too long since last bcm reading, assuming beam off in between
+                if previous == 0: previous = dt
+                if dt - previous > timedelta(seconds=60): # too long since last bcm reading, assuming beam off in between
                     previous = dt
                     continue
-                sum_charge += bcms[dt]*(dt-previous)     # summming nanocoulombs by time
-            event['dose'] = sum_charge*e_per_nc/raster_area
+                if bcms[dt]<1: continue # skip tiny readings
+                sum_charge += bcms[dt]*(dt-previous).total_seconds()     # summing nanocoulombs by time
+                print(bcms[dt],(dt-previous).total_seconds())
 
-
+            selected[event]['dose'] = sum_charge*e_per_nc/raster_area
+            print(event, selected[event]['dose'])
+    quit()
 
     # go through scattering runs and get Pt using Pb, put in same file
 
@@ -108,7 +115,7 @@ def read_runs():
                 entry = {}
                 date, run, start, stop, cell = match
                 entry['cell'] = cell
-                entry['start_time'] = parser.parse(date+" "+start)
+                entry['start_time'] = parser.parse(date+" "+start).replace(tzinfo=eastern)
                 if start.split(":")[0] > stop.split(":")[0]:     # handle runs that end on next day
                     dt = parser.parse(date+" "+stop).replace(tzinfo=eastern)
                     entry['stop_time'] = dt + timedelta(days=1)
@@ -151,45 +158,49 @@ def read_scattering():
                 run_dict[run] = pol
     return run_dict
 
-def select_events(settings, begin, end):
-    '''Return events from datetime range given'''
+
+def get_events(settings):
+
     eastern = tz.gettz('US/Eastern')
     utc = tz.gettz('UTC')
-
-    #begin = parser.parse(begin).replace(tzinfo=eastern)
-    #end = parser.parse(end).replace(tzinfo=eastern)
     filename_regex = re.compile(
         '(\d{4}-\d{2}-\d{2}_\d{2}-\d{2}-\d{2})__(\d{4}-\d{2}-\d{2}_\d{2}-\d{2}-\d{2}).txt')
     all_files = glob.glob(f"{settings['proton_data_dir']}/*.txt") \
-                     + glob.glob(f"{settings['deuteron_data_dir']}/*.txt")
-    included = []
-
-    for file in sorted(all_files):
-        if 'current' in file or 'baseline' in file:
-            continue
-        else:
-            m = filename_regex.search(file)
-            if not m: continue  # skip if we don't match the regex
-            start = m.groups()[0]
-            stop = m.groups()[1]
-            start_dt = datetime.strptime(start, "%Y-%m-%d_%H-%M-%S").replace(tzinfo=utc)
-            stop_dt = datetime.strptime(stop, "%Y-%m-%d_%H-%M-%S").replace(tzinfo=utc)
-            if start_dt < end < stop_dt or start_dt< begin < stop_dt:
-                included.append(file)
+                + glob.glob(f"{settings['deuteron_data_dir']}/*.txt")
     events = {}
-    for eventfile in included:
-        print('Parsing file:', eventfile)
-        with open(eventfile, 'r') as f:
-            for line in f:
-                event = json.loads(line)
-                s = event['stop_time']
-                line_stoptime = datetime.strptime(s[:26], '%Y-%m-%d %H:%M:%S.%f').replace(tzinfo=utc)
-                # utcstamp = str(event['stop_stamp'])
-                #utcstamp = parser.parse(event['stop_stamp']).replace(tzinfo=utc)
-                if begin < line_stoptime < end and 'pol' in event:
-                    events[line_stoptime] = event  # full dictionary from datafile
-                    events[line_stoptime]['stop_time'] = line_stoptime  # full dictionary from datafile
+
+    shelf = shelve.open('event_shelf')
+    if 'events' in shelf:         # if already in shelf, return that, otherwise read from file
+        events.update(shelf['events'])
+    else:
+        print("Filling shelf")
+        for eventfile in all_files:
+            if 'base' in eventfile or 'current' in eventfile: continue
+            print('Parsing file:', eventfile)
+            with open(eventfile, 'r') as f:
+                for line in f:
+                    event = json.loads(line)
+                    s = event['stop_time']
+                    event['start_dt'] = parser.parse(event['start_time']).replace(tzinfo=utc)
+                    event['stop_dt'] = parser.parse(event['stop_time']).replace(tzinfo=utc)
+                    events[event['stop_dt']] = event
+
+        print("Done filling shelf")
+        shelf['events'] = events
+        shelf.close()
     return events
+
+def select_events(events, begin, end):
+    '''Return events from datetime range given'''
+    eastern = tz.gettz('US/Eastern')
+    utc = tz.gettz('UTC')
+    selected_events = []
+
+    for event_dt in sorted(events.keys()):
+        dt = events[event_dt]['stop_dt']
+        if begin <  dt < end and 'pol' in event:
+            selected_events[dt] = events[event_dt]  # full dictionary from datafile
+    return selected_events
 
 if __name__ == '__main__':
     main()
